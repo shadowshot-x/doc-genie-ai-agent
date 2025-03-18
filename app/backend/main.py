c24fb84bdd6027cf14b2db6b3739065904b056eb
ujjwal sharma <ujjwal26599@gmail.com>
2025-03-18 21:57:17 +0530

    Custom Node + Refactor

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,41 +1,6 @@
-from langchain_ollama import ChatOllama
-from typing import Annotated
-from typing_extensions import TypedDict
-from langgraph.graph import StateGraph
-from langgraph.graph.message import add_messages
-from langgraph.prebuilt import ToolNode, tools_condition
-from tools import tool
+from graph import graph
 
-class State(TypedDict):
-    messages: Annotated[list, add_messages]
-
-
-graph_builder = StateGraph(State)
-
-
-tool = tool.querycsv
-tools = [tool]
-llm = ChatOllama(model="granite3.2:2b")
-llm_with_tools = llm.bind_tools(tools)
-
-
-def chatbot(state: State):
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
-
-
-graph_builder.add_node("chatbot", chatbot)
-
-tool_node = ToolNode(tools=[tool])
-graph_builder.add_node("tools", tool_node)
-
-graph_builder.add_conditional_edges(
-    "chatbot",
-    tools_condition,
-)
-# Any time a tool is called, we return to the chatbot to decide the next step
-graph_builder.add_edge("tools", "chatbot")
-graph_builder.set_entry_point("chatbot")
-graph = graph_builder.compile()
+graph = graph.init_graph()
 
 def stream_graph_updates(user_input: str):
     for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
@@ -50,9 +15,7 @@ while True:
             break
 
         stream_graph_updates(user_input)
-    except:
+    except Exception as e:
+        print(e)
         # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
         break
